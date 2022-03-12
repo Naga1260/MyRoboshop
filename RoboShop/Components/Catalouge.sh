7fd6171c785af3dd0c5880b738ab0c6527280583
Naga1260 <tulanagarjuna@gmail.com>
2022-03-12 23:24:30 +0800

    Update frontend

--- RoboShop/Components/Catalouge.sh
+++ RoboShop/Components/Catalouge.sh
@@ -13,11 +13,16 @@ yum install nodejs gcc-c++ -y  &>>LOG_FILE
 check $?
 
 print "create a roboshop user"
-useradd roboshop &>>LOG_FILE
+robo_id=$(id roboshop)
+if [ "$robo_id" -eq 0 ]; then
+  echo -e "\e[31mUser existed\e[0m"
+else
+ useradd roboshop
+fi
 check $?
 
 print "sudo to the user roboshop"
-su - roboshop
+su roboshop
 print "download the catalogue"
 curl -f -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>LOG_FILE
 check $?
@@ -30,8 +35,11 @@ print "install npm"
 npm install &>>LOG_FILE
 check $?
 
-print "move to centos user"
-su - centos
+print "move to root user"
+logout
+check $?
+print "Updte Mongo_dns"
+sed -i -e 's/MONGO_DNSNAME/172.31.83.93/' /home/roboshop/catalogue/systemd.service
 check $?
 
 print "update the catalogue config to systemd"
