5f7eed47eb93855a5888b89eec51257c213c7c41
Naga1260 <tulanagarjuna@gmail.com>
2022-03-12 16:13:41 +0800

    Update frontend

--- .idea/Components/frontend.sh
+++ .idea/Components/frontend.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ id -u != 0]; then
-  echo run the script with sudo
+  echo "run the script with sudo"
 fi
 
 yum install nginx -y
@@ -21,7 +21,7 @@ if [ echo $? == 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
 else
   echo -e "\e[31mFAILURE\e[0m"
-   exit 2
+  exit 2
 
 curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 if [ echo $? == 0 ]; then
