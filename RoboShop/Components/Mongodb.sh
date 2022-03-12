25978a757b065897e6e5be803dc515fefe0d4882
Naga1260 <tulanagarjuna@gmail.com>
2022-03-12 22:04:40 +0800

    Update frontend

--- RoboShop/Components/Mongodb.sh
+++ RoboShop/Components/Mongodb.sh
@@ -4,19 +4,19 @@ source Components/common.sh
 
 echo this is Mongodb
 print "Downloading files"
-curl -f -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
+curl -f -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>LOG_FILE
 check $?
 
 print "Install MongoDB"
-yum install -y mongodb-org $>>LOG_FILE
+yum install -y mongodb-org &>>LOG_FILE
 check $?
 
 print "Enable MongoDB Service"
-systemctl enable mongod $>>LOG_FILE
+systemctl enable mongod &>>LOG_FILE
 check $?
 
 print "Start MongoDB service"
-systemctl start mongod $>>LOG_FILE
+systemctl start mongod &>>LOG_FILE
 check $?
 
 print "Update mongodb listen address"
@@ -24,15 +24,15 @@ sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 check $?
 
 print "Restart MongoDB service"
-systemctl restart mongod $>>LOG_FILE
+systemctl restart mongod &>>LOG_FILE
 check $?
 
 print "Downloading mongodb  content"
-curl -f -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" $>>LOG_FILE
+curl -f -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>LOG_FILE
 check $?
 
 print "move the content"
-cd /tmp $>>LOG_FILE && unzip mongodb.zip $>>LOG_FILE && cd mongodb-main $>>LOG_FILE
+cd /tmp &>>LOG_FILE && unzip mongodb.zip &>>LOG_FILE && cd mongodb-main &>>LOG_FILE
 check $?
 print "update database"
 mongo < catalogue.js
--- RoboShop/Components/frontend.sh
+++ RoboShop/Components/frontend.sh
@@ -3,25 +3,25 @@
 source Components/common.sh
 
 print "Installing NGINX package"
-yum install nginx -y $>>LOG_FILE
+yum install nginx -y &>>LOG_FILE
 check $?
 print "Enable NGINX service"
-systemctl enable nginx $>>LOG_FILE
+systemctl enable nginx &>>LOG_FILE
 check $?
 print "START NGINX Service"
-systemctl start nginx $>>LOG_FILE
+systemctl start nginx &>>LOG_FILE
 check $?
 print "Download the frontend content"
-curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" $>>LOG_FILE
+curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>LOG_FILE
 check $?
 print "manage the content"
 cd /usr/share/nginx/html
 rm -rf *
-unzip /tmp/frontend.zip $>>LOG_FILE && mv frontend-main/* .  $>>LOG_FILE && mv static/* . $>>LOG_FILE
+unzip /tmp/frontend.zip &>>LOG_FILE && mv frontend-main/* .  &>>LOG_FILE && mv static/* . &>>LOG_FILE
 check $?
 print "copy config file"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 check $?
 print "restrat NGINX service"
-systemctl restart nginx $>>LOG_FILE
+systemctl restart nginx &>>LOG_FILE
 check $?
