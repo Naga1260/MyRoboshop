2363ec9ae3ec0184332469b406c790469ef868ea
Naga1260 <tulanagarjuna@gmail.com>
2022-03-12 17:30:01 +0800

    Update frontend

--- RoboShop/Components/frontend.sh
+++ RoboShop/Components/frontend.sh
@@ -2,26 +2,26 @@
 
 source Components/common.sh
 
-echo Installing NGINX package
+echo -e "\e[34mInstalling NGINX package\e[0m"
 yum install nginx -y
 check $?
-echo Enable NGINX service
+echo -e "\e[34mEnable NGINX service\e[0m"
 systemctl enable nginx
 check $?
-echo START NGINX Service
+echo -e "\e[34mSTART NGINX Service\e[0m"
 systemctl start nginx
 check $?
-echo Download the frontend content
+echo -e "\e[34mDownload the frontend content\e[0m"
 curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 chec $?
-echo manage the content
+echo -e "\e[34mmanage the content\e[0m"
 cd /usr/share/nginx/html
 rm -rf *
 unzip /tmp/frontend.zip && mv frontend-main/* . && mv static/* .
 check $?
-echo copy config file
+echo -e "\e[34mcopy config file\e[0m"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 check $?
-echo restrat NGINX service
+echo -e "\e[34mrestrat NGINX service\e[0m"
 systemctl restart nginx
 check $?
