c306bada6ffb4303c7a62d474b250c4c5d6c9f47
Naga1260 <tulanagarjuna@gmail.com>
2022-03-12 16:22:41 +0800

    Update frontend

--- .idea/Components/frontend.sh
+++ .idea/Components/frontend.sh
@@ -6,7 +6,7 @@ if [ $USER_ID -ne 0]; then
   exit 1
 fi
 
-echo Installing NGNIX package
+echo Installing NGINX package
 yum install nginx -y
 if [ $? -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
@@ -14,6 +14,7 @@ else
   echo -e "\e[31mFAILURE\e[0m"
   exit 2
 fi
+echo Enable NGINX service
 systemctl enable nginx
 if [  $? -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
@@ -21,6 +22,7 @@ else
   echo -e "\e[31mFAILURE\e[0m"
   exit 2
 fi
+echo START NGINX Service
 systemctl start nginx
 if [ $? -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
@@ -28,6 +30,7 @@ else
   echo -e "\e[31mFAILURE\e[0m"
   exit 2
 fi
+echo Download the frontend content
 curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 if [ $? -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
@@ -35,6 +38,7 @@ else
   echo -e "\e[31mFAILURE\e[0m"
   exit 2
 fi
+echo manage the content
 cd /usr/share/nginx/html
 rm -rf *
 unzip /tmp/frontend.zip && mv frontend-main/* . && mv static/* .
@@ -44,6 +48,7 @@ else
   echo -e "\e[31mFAILURE\e[0m"
   exit 2
 fi
+echo copy config file
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 if [ $? -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
@@ -51,6 +56,7 @@ else
   echo -e "\e[31mFAILURE\e[0m"
   exit 2
 fi
+echo restrat NGINX service
 systemctl restart nginx
 if [ $? -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
