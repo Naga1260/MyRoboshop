473c91dd9adf22e64dc37ede6ca5404c9c841358
Naga1260 <tulanagarjuna@gmail.com>
2022-03-12 16:19:51 +0800

    Update frontend

--- .idea/Components/frontend.sh
+++ .idea/Components/frontend.sh
@@ -13,26 +13,28 @@ if [ $? -eq 0 ]; then
 else
   echo -e "\e[31mFAILURE\e[0m"
   exit 2
+fi
 systemctl enable nginx
 if [  $? -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
 else
   echo -e "\e[31mFAILURE\e[0m"
   exit 2
+fi
 systemctl start nginx
 if [ $? -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
 else
   echo -e "\e[31mFAILURE\e[0m"
   exit 2
-
+fi
 curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 if [ $? -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
 else
   echo -e "\e[31mFAILURE\e[0m"
   exit 2
-
+fi
 cd /usr/share/nginx/html
 rm -rf *
 unzip /tmp/frontend.zip && mv frontend-main/* . && mv static/* .
@@ -41,15 +43,18 @@ if [ $? -eq 0 ]; then
 else
   echo -e "\e[31mFAILURE\e[0m"
   exit 2
+fi
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 if [ $? -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
 else
   echo -e "\e[31mFAILURE\e[0m"
   exit 2
+fi
 systemctl restart nginx
 if [ $? -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
 else
   echo -e "\e[31mFAILURE\e[0m"
   exit 2
+fi
