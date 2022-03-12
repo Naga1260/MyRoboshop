e5e3b8c43a0074772c31fdb1af939961084ff4c6
Naga1260 <tulanagarjuna@gmail.com>
2022-03-12 15:58:17 +0800

    Forntend component added

--- .idea/Components/frontend.sh
+++ .idea/Components/frontend.sh
@@ -1,15 +1,25 @@
+
+if [ id -u != 0]; then
+  echo run the script with sudo
+fi
+
+
 yum install nginx -y
+echo $?
 systemctl enable nginx
+echo $?
 systemctl start nginx
+echo$?
 
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-
+curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+echo $?
 cd /usr/share/nginx/html
 rm -rf *
 unzip /tmp/frontend.zip
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
+echo $?
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
-
+echo$?
 systemctl restart nginx
