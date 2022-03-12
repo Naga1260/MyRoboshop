6eac5088de635c677dfa48a053132ecea0efa240
Naga1260 <tulanagarjuna@gmail.com>
2022-03-12 23:46:02 +0800

    Update frontend

--- RoboShop/Components/Catalouge.sh
+++ RoboShop/Components/Catalouge.sh
@@ -23,26 +23,23 @@ check $?
 
 print "change user to roboshop"
 su - roboshop
-check $?
 
-print "download the catalogue"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
-check $?
 
-print "move the content and unzip"
+
 cd /home/roboshop &>>LOG_FILE && unzip /tmp/catalogue.zip &>>LOG_FILE && mv catalogue-main catalogue &>>LOG_FILE && cd /home/roboshop/catalogue &>>LOG_FILE
-check $?
 
-print "install npm"
+
+
 npm install &>>LOG_FILE
-check $?
 
-print "move to root user"
-exit
-check $?
-print "Updte Mongo_dns"
+
+
+
 sed -i -e 's/MONGO_DNSNAME/172.31.83.93/' /home/roboshop/catalogue/systemd.service
-check $?
+
+
+exit
 
 print "update the catalogue config to systemd"
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
--- RoboShop/Components/user.sh
+++ RoboShop/Components/user.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
 echo this is USER
+
+print "change user to roboshop"
+su - roboshop
+check $?
+
+print "download the catalogue"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+check $?
+
+print "move the content and unzip"
+cd /home/roboshop &>>LOG_FILE && unzip /tmp/catalogue.zip &>>LOG_FILE && mv catalogue-main catalogue &>>LOG_FILE && cd /home/roboshop/catalogue &>>LOG_FILE
+check $?
+
+print "install npm"
+npm install &>>LOG_FILE
+check $?
+
+
+check $?
+print "Updte Mongo_dns"
+sed -i -e 's/MONGO_DNSNAME/172.31.83.93/' /home/roboshop/catalogue/systemd.service
+check $?
