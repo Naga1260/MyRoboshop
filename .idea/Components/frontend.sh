3a0c4970feffe960ae51af4a3e1bd650060a681d
Naga1260 <tulanagarjuna@gmail.com>
2022-03-12 16:12:10 +0800

    Update frontend

--- .idea/Components/frontend.sh
+++ .idea/Components/frontend.sh
@@ -1,29 +1,30 @@
+#!/bin/bash
 
 if [ id -u != 0]; then
   echo run the script with sudo
 fi
 
 yum install nginx -y
-if [echo $? == 0]; then
+if [ echo $? == 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
 else
   echo -e "\e[31mFAILURE\e[0m"
   exit 2
 systemctl enable nginx
-if [echo $? == 0]; then
+if [ echo $? == 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
 else
   echo -e "\e[31mFAILURE\e[0m"
   exit 2
 systemctl start nginx
-if [echo $? == 0]; then
+if [ echo $? == 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
 else
   echo -e "\e[31mFAILURE\e[0m"
    exit 2
 
 curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-if [ echo $? == 0]; then
+if [ echo $? == 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
 else
   echo -e "\e[31mFAILURE\e[0m"
@@ -44,7 +45,7 @@ else
   echo -e "\e[31mFAILURE\e[0m"
   exit 2
 systemctl restart nginx
-if [echo $? == 0 ]; then
+if [ echo $? == 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
 else
   echo -e "\e[31mFAILURE\e[0m"
