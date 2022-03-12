62073ace266681fa9e224b9120a4c4de4f3e8335
Naga1260 <tulanagarjuna@gmail.com>
2022-03-12 23:50:18 +0800

    Update frontend

--- RoboShop/Components/Catalouge.sh
+++ RoboShop/Components/Catalouge.sh
@@ -17,29 +17,29 @@ id -u roboshop
 if [ "$?" -eq 0 ]; then
   echo -e "\e[31mUser existed\e[0m"
 else
- useradd roboshop
+  useradd roboshop
 fi
 check $?
 
 print "change user to roboshop"
 su - roboshop
+check $?
 
+print "download the catalogue"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+check $?
 
-
+print "move the content and unzip"
 cd /home/roboshop &>>LOG_FILE && unzip /tmp/catalogue.zip &>>LOG_FILE && mv catalogue-main catalogue &>>LOG_FILE && cd /home/roboshop/catalogue &>>LOG_FILE
+check $?
 
-
-
+print "install npm"
 npm install &>>LOG_FILE
+check $?
 
-
-
-
+print "Update Mongo_dns"
 sed -i -e 's/MONGO_DNSNAME/172.31.83.93/' /home/roboshop/catalogue/systemd.service
-
-
-exit
+check $?
 
 print "update the catalogue config to systemd"
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
