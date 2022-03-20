8372562a5e88d7b89bfff3ccbfea4bb25d9f0c83
Naga1260 <tulanagarjuna@gmail.com>
2022-03-20 15:56:40 +0800

    Update frontend

--- RoboShop/Components/create-ec2.sh
+++ RoboShop/Components/create-ec2.sh
@@ -13,5 +13,10 @@ security_group=$(aws ec2 describe-security-groups --filters Name=group-name,Valu
 echo $AMI_ID
 echo $security_group
 
-
-#aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
+aws ec2 run-instances \
+  --image-id ${AMI_ID} \
+  --instance-type t2.micro \
+  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
+  --security-group-ids $security_group \
+  --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
+  | jq
