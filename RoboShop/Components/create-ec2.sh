ff42e2d8bc8a8d72832cf1b527635c342a1da827
Naga1260 <tulanagarjuna@gmail.com>
2022-03-20 11:40:01 +0800

    Update frontend

--- RoboShop/Components/create-ec2.sh
+++ RoboShop/Components/create-ec2.sh
@@ -11,4 +11,5 @@ AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Pra
 
 echo $AMI_ID
 
+
 aws ec2 run-instance --image-id ${AMI_ID} --instance-type t2.micro ResourceType=instance,Tags=[{Key=Name,Value={$COMPONENT}}]
