c40c1c75be7ca34a0617acfd57baa46155be3f0a
Naga1260 <tulanagarjuna@gmail.com>
2022-03-20 11:59:07 +0800

    Update frontend

--- RoboShop/Components/create-ec2.sh
+++ RoboShop/Components/create-ec2.sh
@@ -13,4 +13,5 @@ seciryt-group=$(aws ec2 describe-security-groups --filters Name=group-name,Value
 echo $AMI_ID
 echo $secutiy-group
 
+
 aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
