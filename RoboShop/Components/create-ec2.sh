575f8bab54f0095107e6ea9c70261ae4edf81089
Naga1260 <tulanagarjuna@gmail.com>
2022-03-20 12:02:44 +0800

    Update frontend

--- RoboShop/Components/create-ec2.sh
+++ RoboShop/Components/create-ec2.sh
@@ -8,10 +8,10 @@ fi
 COMPONENT=$1
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
-security-group=$(aws ec2 describe-security-groups --filters Name=group-name,Values=Allow-All-from-Public | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
+security_group=$(aws ec2 describe-security-groups --filters Name=group-name,Values=Allow-All-from-Public | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 
 echo $AMI_ID
-echo $(security-group)
+echo $(security_group)
 
 
 #aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
